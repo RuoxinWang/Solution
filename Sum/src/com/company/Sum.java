a2c315668fedab263dd0361d0d936d3797dac167
RuoxinWang <rwang0104@gmail.com>
2021-10-05 18:13:51 -0700

    Ruoxin Wang_Info 5100_HW2_Sum

---
+++ Sum/src/com/company/Sum.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class Sum {
+    public int add(int num1, int num2){
+        int result = 0;
+        result = num1 + num2;
+        return result;
+    }
+    public int add(int num1, int num2, int num3) {
+        int result = 0;
+        result = num1 + num2 + num3;
+        return result;
+    }
+    public double add(double num1, double num2){
+        double result = 0;
+        result = num1 + num2;
+        return result;
+    }
+}
+
+
