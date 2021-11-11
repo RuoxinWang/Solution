ea6926d794edfb34b568f88ea14adcb143ec3cb9
RuoxinWang <rwang0104@gmail.com>
2021-11-10 16:00:23 -0800

    HW4 Question2

---
+++ HW4Question2/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //cars = {1,3,5,2,4};
+        //directions = {2,1,2,4,3};
+        //arrivalTimes = {10,20,30,40,50};
+
+
+    }
+}
